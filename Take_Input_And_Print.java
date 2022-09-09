6c4fdb3242e2709daf2cd3d3e7a36d2b3d12527c
aparna yendru <97721064+aparna21A91A6137@users.noreply.github.com>
2022-09-09 11:18:54 +0530

    import java.util.Scanner; class Average {     public static void main(String args[])     {         String a;         Scanner sc=new Scanner(System.in);         a=sc.nextLine();         System.out.println(a);         sc.close();     } }

---
+++ Take_Input_And_Print.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+class Average
+{
+    public static void main(String args[])
+    {
+        String a;
+        Scanner sc=new Scanner(System.in);
+        a=sc.nextLine();
+        System.out.println(a);
+        sc.close();
+    }
+}
