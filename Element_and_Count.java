ac045315f25e42b3587f8f2524dbedc6b9a60855
aparna yendru <97721064+aparna21A91A6137@users.noreply.github.com>
2022-09-23 16:56:33 +0530

    import java.util.Scanner; class Minimum {     public static void main(String args[])     {         int n,count=0,countt=0,sum=0,y=0;         float avrg;         Scanner sc=new Scanner(System.in);         n=sc.nextInt();         int[] arr=new int[n];         for(int i=0;i<n;i++)         {             arr[i]=sc.nextInt();         }         for(int i=0;i<n;i++)         {            count=1;            if(arr[i]==-1)            {                continue;            }            for(int j=i+1;j<n;j++)            {                if(arr[i]==arr[j])                {                    count++;                    arr[j]=-1;                }            }            System.out.print(arr[i]+" ");            System.out.print(count+" ");         }         sc.close();     } }

---
+++ Element_and_Count.java
@@ -0,0 +1,35 @@
+import java.util.Scanner;
+class Minimum
+{
+    public static void main(String args[])
+    {
+        int n,count=0,countt=0,sum=0,y=0;
+        float avrg;
+        Scanner sc=new Scanner(System.in);
+        n=sc.nextInt();
+        int[] arr=new int[n];
+        for(int i=0;i<n;i++)
+        {
+            arr[i]=sc.nextInt();
+        }
+        for(int i=0;i<n;i++)
+        {
+           count=1;
+           if(arr[i]==-1)
+           {
+               continue;
+           }
+           for(int j=i+1;j<n;j++)
+           {
+               if(arr[i]==arr[j])
+               {
+                   count++;
+                   arr[j]=-1;
+               }
+           }
+           System.out.print(arr[i]+" ");
+           System.out.print(count+" ");
+        }
+        sc.close();
+    }
+}
