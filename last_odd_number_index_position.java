d966390149eaef9dc74cd3157e21619661ef1fca
aparna yendru <97721064+aparna21A91A6137@users.noreply.github.com>
2022-09-23 20:02:21 +0530

    import java.util.Scanner; class Minimum {     public static void main(String args[])     {         int n,count=0,countt=0,sum=0,y=0;         Scanner sc=new Scanner(System.in);         n=sc.nextInt();         int[] arr=new int[n];         for(int i=0;i<n;i++)         {             arr[i]=sc.nextInt();         }         for(int i=n-1;i>=0;i--)         {             if(arr[i]%2!=0)             {                 System.out.print(i);                 break;             }         }         sc.close();     } }

---
+++ last_odd_number_index_position.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+class Minimum
+{
+    public static void main(String args[])
+    {
+        int n,count=0,countt=0,sum=0,y=0;
+        Scanner sc=new Scanner(System.in);
+        n=sc.nextInt();
+        int[] arr=new int[n];
+        for(int i=0;i<n;i++)
+        {
+            arr[i]=sc.nextInt();
+        }
+        for(int i=n-1;i>=0;i--)
+        {
+            if(arr[i]%2!=0)
+            {
+                System.out.print(i);
+                break;
+            }
+        }
+        sc.close();
+    }
+}
