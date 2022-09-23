b7c60740436dd8243048e0b4b634c847bffd45fc
aparna yendru <97721064+aparna21A91A6137@users.noreply.github.com>
2022-09-23 13:18:35 +0530

    import java.util.Scanner; class Minimum {     public static void main(String args[])     {         int n,y=0,sum=0;         Scanner sc=new Scanner(System.in);         n=sc.nextInt();         int[] arr=new int[n];         for(int i=0;i<n;i++)         {             arr[i]=sc.nextInt();         }         int a=sc.nextInt();         int b=sc.nextInt();         for(int i=0;i<n;i++)         {             if(arr[i]>=a && arr[i]<=b)             {                 y=1;                 sum=sum+arr[i];             }         }         if(y==0)         {             System.out.println("-1");         }         else         {             System.out.println(sum);         }         sc.close();     } }

---
+++ sum_of_elements_between_A_and_B.java
@@ -0,0 +1,34 @@
+import java.util.Scanner;
+class Minimum
+{
+    public static void main(String args[])
+    {
+        int n,y=0,sum=0;
+        Scanner sc=new Scanner(System.in);
+        n=sc.nextInt();
+        int[] arr=new int[n];
+        for(int i=0;i<n;i++)
+        {
+            arr[i]=sc.nextInt();
+        }
+        int a=sc.nextInt();
+        int b=sc.nextInt();
+        for(int i=0;i<n;i++)
+        {
+            if(arr[i]>=a && arr[i]<=b)
+            {
+                y=1;
+                sum=sum+arr[i];
+            }
+        }
+        if(y==0)
+        {
+            System.out.println("-1");
+        }
+        else
+        {
+            System.out.println(sum);
+        }
+        sc.close();
+    }
+}
