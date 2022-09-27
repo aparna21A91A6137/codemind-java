c0b6733e6f830e72b2f17708d230a9abbc32e8e6
aparna yendru <97721064+aparna21A91A6137@users.noreply.github.com>
2022-09-27 20:12:04 +0530

    import java.util.Scanner; class Odd {     public static void main(String args[])     {         int count=0,y=0,sum=0,i=0;         float avrg;         Scanner sc=new Scanner(System.in);         int n=sc.nextInt();         int [] arr=new int[n];         for(i=0;i<n;i++)         {             arr[i]=sc.nextInt();         }         int min=arr[0];         for(i=1;i<n;i++)         {             if(arr[i]<min)             {                 min=arr[i];             }         }         System.out.print(min);     } }

---
+++ minimum_element_of_an_array.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+class Odd
+{
+    public static void main(String args[])
+    {
+        int count=0,y=0,sum=0,i=0;
+        float avrg;
+        Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt();
+        int [] arr=new int[n];
+        for(i=0;i<n;i++)
+        {
+            arr[i]=sc.nextInt();
+        }
+        int min=arr[0];
+        for(i=1;i<n;i++)
+        {
+            if(arr[i]<min)
+            {
+                min=arr[i];
+            }
+        }
+        System.out.print(min);
+    }
+}
