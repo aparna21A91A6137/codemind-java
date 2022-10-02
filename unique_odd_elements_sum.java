3741e0a7f961d4bfa2f7dcab45630c031e64b82d
aparna yendru <97721064+aparna21A91A6137@users.noreply.github.com>
2022-10-02 14:21:41 +0530

    import java.util.Scanner; class Wave {     public static void main(String args[])     {         int y=0;         Scanner sc=new Scanner(System.in);         int n=sc.nextInt();         int[] arr=new int[n];         for(int i=0;i<n;i++)         {             arr[i]=sc.nextInt();         }         for(int i=0;i<n;i++)         {             int count=0;             for(int j=i+1;j<n;j++)             {                 if(arr[i]==arr[j])                 {                     count++;                 }             }             if(count==0)             {                 if(arr[i]%2!=0)                 {                     y+=arr[i];                 }             }         }         System.out.print(y);     } }

---
+++ unique_odd_elements_sum.java
@@ -0,0 +1,34 @@
+import java.util.Scanner;
+class Wave
+{
+    public static void main(String args[])
+    {
+        int y=0;
+        Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt();
+        int[] arr=new int[n];
+        for(int i=0;i<n;i++)
+        {
+            arr[i]=sc.nextInt();
+        }
+        for(int i=0;i<n;i++)
+        {
+            int count=0;
+            for(int j=i+1;j<n;j++)
+            {
+                if(arr[i]==arr[j])
+                {
+                    count++;
+                }
+            }
+            if(count==0)
+            {
+                if(arr[i]%2!=0)
+                {
+                    y+=arr[i];
+                }
+            }
+        }
+        System.out.print(y);
+    }
+}
