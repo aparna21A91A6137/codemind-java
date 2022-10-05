657e34c3764d0f18cbc430de7945573467a1b28b
aparna yendru <97721064+aparna21A91A6137@users.noreply.github.com>
2022-10-05 11:19:40 +0530

    import java.util.Scanner; class Tables {     public static void main(String args[])     {         Scanner sc=new Scanner(System.in);         int n=sc.nextInt();         int sum=0,r;         while(sum!=1&&sum!=4)         {         sum=0;         while(n>0)         {             r=n%10;             sum=sum+r*r;             n=n/10;         }         n=sum;         }         if(sum==1)         {         System.out.print("True");         }         else         {         System.out.print("False");         }     } }

---
+++ happy_number.java
@@ -0,0 +1,29 @@
+import java.util.Scanner;
+class Tables
+{
+    public static void main(String args[])
+    {
+        Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt();
+        int sum=0,r;
+        while(sum!=1&&sum!=4)
+        {
+        sum=0;
+        while(n>0)
+        {
+            r=n%10;
+            sum=sum+r*r;
+            n=n/10;
+        }
+        n=sum;
+        }
+        if(sum==1)
+        {
+        System.out.print("True");
+        }
+        else
+        {
+        System.out.print("False");
+        }
+    }
+}
