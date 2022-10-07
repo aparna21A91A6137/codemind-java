4ef6b3682df52e93a7eb251f5156af6c6b301bc8
aparna yendru <97721064+aparna21A91A6137@users.noreply.github.com>
2022-10-07 15:06:21 +0530

    import java.util.Scanner; class Unique {     public static void main(String args[])     {         Scanner sc=new Scanner(System.in);         int n=sc.nextInt();         int count=0,y=0,countt=0;         int[] arr=new int[n];         for(int i=0;i<n;i++)         {             arr[i]=sc.nextInt();         }         for(int i=0;i<n;i++)         {             count=0;             for(int j=i+1;j<n;j++)             {                 if(arr[i]==arr[j])                 {                     count++;                 }             }             if(count>countt)             {                 countt=count;                 y=arr[i];             }         }         System.out.print(y);     } }

---
+++ Majority_Element.java
@@ -0,0 +1,32 @@
+import java.util.Scanner;
+class Unique
+{
+    public static void main(String args[])
+    {
+        Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt();
+        int count=0,y=0,countt=0;
+        int[] arr=new int[n];
+        for(int i=0;i<n;i++)
+        {
+            arr[i]=sc.nextInt();
+        }
+        for(int i=0;i<n;i++)
+        {
+            count=0;
+            for(int j=i+1;j<n;j++)
+            {
+                if(arr[i]==arr[j])
+                {
+                    count++;
+                }
+            }
+            if(count>countt)
+            {
+                countt=count;
+                y=arr[i];
+            }
+        }
+        System.out.print(y);
+    }
+}
