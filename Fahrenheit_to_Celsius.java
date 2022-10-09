46c1ce390c5fa62c69803d4b385c7bdeea7d9247
aparna yendru <97721064+aparna21A91A6137@users.noreply.github.com>
2022-10-09 14:30:08 +0530

    import java.util.Scanner; class Division {     public static void main(String args[])     {         int f;         float c;         Scanner sc=new Scanner(System.in);         f=sc.nextInt();         c=(float)((f-32)*5)/9;         System.out.format("%.2f",c);         sc.close();     } }

---
+++ Fahrenheit_to_Celsius.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+class Division
+{
+    public static void main(String args[])
+    {
+        int f;
+        float c;
+        Scanner sc=new Scanner(System.in);
+        f=sc.nextInt();
+        c=(float)((f-32)*5)/9;
+        System.out.format("%.2f",c);
+        sc.close();
+    }
+}
